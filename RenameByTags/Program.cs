65284fc61a8c642a742f867e6f7ef22ed0a73777
adkurylev <adkurylev@gmail.com>
2019-07-07 18:11:54 +0300

    Initializing commit

---
+++ RenameByTags/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameByTags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(Directory.GetCurrentDirectory()).Where(x => Path.GetExtension(x) == ".mp3");
+
+            foreach (var file in files)
+            {
+                string artist, title, name;
+
+                TagLib.File f = TagLib.File.Create(file);
+
+                artist = f.Tag.FirstPerformer;
+                title = f.Tag.Title;
+
+                f.Dispose();
+
+                if (artist == null || title == null)
+                    continue;
+
+                name = $"{artist} - {title}.mp3";
+
+                File.Move(file, $"{name}");
+            }
+
+        }
+    }
+}
