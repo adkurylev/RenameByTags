7d9acf72e1167c5a403c9a14914b27f8e9c16c40
adkurylev <adkurylev@gmail.com>
2019-07-19 14:01:15 +0300

    Directory choice was added

--- RenameByTags/Program.cs
+++ RenameByTags/Program.cs
@@ -1,37 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace RenameByTags
+namespace RenameByTagsMP3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IEnumerable<string> files = Directory.EnumerateFiles(Directory.GetCurrentDirectory()).Where(x => Path.GetExtension(x) == ".mp3");
+            System.Console.WriteLine("Хотите также переименовать файлы в подкаталогах?\nY/N");
+            string userAnswer = System.Console.ReadLine();
 
-            foreach (var file in files)
+            IEnumerable<string> directories = null;
+
+            if (userAnswer.ToLower() == "y" || userAnswer.ToLower() == "yes")
             {
-                string artist, title, name;
-
-                TagLib.File f = TagLib.File.Create(file);
-
-                artist = f.Tag.FirstPerformer;
-                title = f.Tag.Title;
-
-                f.Dispose();
-
-                if (artist == null || title == null)
-                    continue;
-
-                name = $"{artist} - {title}.mp3";
-
-                File.Move(file, $"{name}");
+                directories = Directory.EnumerateDirectories(Directory.GetCurrentDirectory()).Concat(new List<string>() { Directory.GetCurrentDirectory() });
+            }
+            else
+            {
+                if (userAnswer.ToLower() == "n" || userAnswer.ToLower() == "no")
+                {
+                    directories = new List<string>() { Directory.GetCurrentDirectory() };
+                }
+                else
+                {
+                    System.Console.WriteLine("Введено неверное значение.");
+                    return;
+                }
             }
 
+            foreach (var dir in directories)
+            {
+                System.Console.ForegroundColor = System.ConsoleColor.Blue;
+                System.Console.WriteLine(dir);
+                System.Console.ForegroundColor = System.ConsoleColor.White;
+
+                IEnumerable<string> files = Directory.EnumerateFiles(dir).Where(x => Path.GetExtension(x) == ".mp3");
+
+                foreach (var file in files)
+                {
+                    string artist, title, name, path;
+
+                    TagLib.File f = TagLib.File.Create(file);
+
+                    artist = f.Tag.FirstPerformer;
+                    title = f.Tag.Title;
+
+                    f.Dispose();
+
+                    if (artist == null || title == null)
+                        continue;
+
+                    name = $"{artist} - {title}.mp3";
+                    path = $"{dir}\\{name}";
+                    System.Console.WriteLine(path);
+                    try
+                    {
+                        File.Move(file, path);
+                    }
+                    catch (System.Exception)
+                    {
+                        System.Console.ForegroundColor = System.ConsoleColor.Red;
+                        System.Console.WriteLine("Переименуйте файл вручную.");
+                        System.Console.ForegroundColor = System.ConsoleColor.White;
+                        System.Console.ReadLine();
+                    }
+                }
+                System.Console.WriteLine();
+            }
+
+            System.Console.ReadLine();
         }
     }
 }
